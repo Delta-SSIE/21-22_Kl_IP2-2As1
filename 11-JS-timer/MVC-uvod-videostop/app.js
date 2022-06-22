87e58a3cc48a76a1b76e0d7760d2bd6cf2ff8ceb
Jan Koupil <jan.koupil@gmail.com>
2022-06-22 10:26:00 +0200

    Hotovy zaklad MVC Videostop

--- 11-JS-timer/MVC-uvod-videostop/app.js
+++ 11-JS-timer/MVC-uvod-videostop/app.js
@@ -1,12 +1,9 @@
 const imgBase = 'https://koupil.delta-www.cz/IP2/videostop/';
+const delay = 250;
 
 const diceModel = {
-    values : [0,0,0],
+    values : [1,1,1],
     nextDice : 0
-}
-
-const controller = {
-
 }
 
 const diceView = {
@@ -24,13 +21,59 @@ const diceView = {
 }
 
 const playBtnView = {
-    init : function() {
-
+    element : null,
+    init : function(controller) {
+        //najit element
+        this.element = document.getElementById('playBtn')
+        this.element.addEventListener("click", function() {controller.playStopPress()})
     },
-    render : function() {
-
+    render : function(isPlay) {
+        this.element.textContent = isPlay ? "Play" : "Stop"
     }
 }
 
+const controller = {
+    timer : null,
+    diceModel : null,
+    diceView : null,
+    playBtnView : null, 
+    init : function(){
+        for (let i = 0; i < 3; i++){
+            this.getNextDice();
+        }
+    },
+    playStopPress : function(){
+        //zapíná - vypíná timer
+        if (this.timer === null) {
+            this.startGame()
+        } else {
+            this.stopGame()
+        }
+    },
+    startGame : function(){
+        const that = this
+        this.timer = window.setInterval( function(){that.getNextDice()}, delay )
+        playBtnView.render(false)
+    },
+    stopGame : function() {
+        window.clearInterval(this.timer)
+        this.timer = null
+        playBtnView.render(true)
+    },
+    getNextDice : function() {
+        const num = Math.floor(Math.random() * 6) + 1
+        diceModel.values[diceModel.nextDice] = num
+        diceModel.nextDice++
+        diceModel.nextDice %= 3
+        diceView.render(diceModel.values)
+    }
+
+}
+
 diceView.init()
-diceView.render([1,1,6])
+playBtnView.init(controller)
+
+controller.diceModel = diceModel
+controller.diceView = diceView
+controller.playBtnView = playBtnView
+controller.init();
